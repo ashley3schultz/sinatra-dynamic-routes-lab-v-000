358e8c5df27f2d0fe1b05be4cf2851c4b7e1ce83
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-13 23:40:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
   get '/square/:number' do
     n = params[:number]
     binding.pry
-    (1..n).detect {|s| s * s == n}
+    (1..n).detect {|s| s * s == n || s * 4 == n}
 
   end
   # Write your code here!
