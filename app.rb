aafbbf9c415a41e244fc442ddf7a8cea48f1d624
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-14 00:48:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,11 +12,11 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    i = 1
+    i = 0
     while i < params[:number].to_i
       "#{params[:phrase]}"
       i += 1
-    end 
+    end
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
