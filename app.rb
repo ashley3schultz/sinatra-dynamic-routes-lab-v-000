f1a0b591480c30479eb2c6dd3eb0f38027744af7
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-14 00:03:10 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,9 +12,7 @@ class App < Sinatra::Base
   end
 
   get 'say/:number/:phrase' do
-    params[:number].to_i.times do
-      params[:phrase]
-    end
+    params[:number].to_i.times { params[:phrase]}
   end
   # Write your code here!
 
